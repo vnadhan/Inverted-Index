36c6d82d69820764d112add3872b235136084100
unknown <vshgemini@gmail.com>
2020-01-09 01:38:26 +0100

    Fixed error in calculating NORM of document for cosine similarity

--- searchengine/SearchEngine.java
+++ searchengine/SearchEngine.java
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -17,6 +18,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+
+import org.apache.commons.math3.linear.MatrixUtils;
 
 public class SearchEngine {
 
@@ -36,6 +39,9 @@ public class SearchEngine {
 
 	// term-document tf-idf mapping
 	private double[][] termDocumentMapping;
+	
+	// inverse of term-document tf-idf mapping
+	private double[][] termDocumentMapping_T;
 
 	// Counter to store the number of documents.
 	private Integer termCounter;
@@ -149,17 +155,16 @@ public class SearchEngine {
 			// Update IDF and TF-IDF for every posting
 			if (postings != null && postings.size() > 0) {
 				// Calculate only if it is not already calculated.
-				// if (postings.get(0).getTf_idf() == null) {
-				// for (Posting posting : postings) {
-				// posting.setInverseDocFreq(documents.size(),
-				// postings.size());
-				// }
-				// }
+//				if (postings.get(0).getTf_idf() == null) {
+//					for (Posting posting : postings) {
+//						posting.setInverseDocFreq(documents.size(), postings.size());
+//					}
+//				}
 
 				// Now sort the instances of Posting according to the respective
 				// values of TF-IDF
 				Collections.sort(postings);
-				
+
 				// Print results
 				// Push the name of the documents into the result list.
 				for (Posting posting : postings) {
@@ -169,7 +174,7 @@ public class SearchEngine {
 
 				// Print the list of relevant documents
 				System.out.println(result);
-				
+
 			} else {
 				// No relevant documents found!
 				System.out.println("[]");
@@ -182,7 +187,8 @@ public class SearchEngine {
 			// continue
 			// with other words in the query.
 
-//			List<Posting> p_for_all_words_in_query = new ArrayList<Posting>();
+			// List<Posting> p_for_all_words_in_query = new
+			// ArrayList<Posting>();
 			Set<Posting> all_postings = new HashSet<Posting>();
 
 			// For each word get the corresponding instances of Posting.
@@ -196,22 +202,24 @@ public class SearchEngine {
 					all_postings.addAll(postings);
 				}
 			}
-			
-//			for(Posting p : all_postings) {
-//				System.out.println(p.getDocument().getId());
-//			}
-			
-			// We have got the relevant documents, now sort it using cosine-similarity method.
+
+			// for(Posting p : all_postings) {
+			// System.out.println(p.getDocument().getId());
+			// }
+
+			// We have got the relevant documents, now sort it using
+			// cosine-similarity method.
 			sortResultsForMultiWordQuery(words, all_postings);
 		}
 	}
 
-	// Sort and display the relevant documents for a multi-word query 
-	private void sortResultsForMultiWordQuery(String[] queryWords, Set<Posting> relevantDocuments) {
+	// Sort and display the relevant documents for a multi-word query
+	private void sortResultsForMultiWordQuery(String[] queryWords,
+			Set<Posting> relevantDocuments) {
 
 		// An index for the words in the query
 		Map<String, TermMetrics> index_for_query = new HashMap<String, TermMetrics>();
-		
+
 		// Temporary variable
 		// IDF for a term
 		Double term_idf;
@@ -228,10 +236,11 @@ public class SearchEngine {
 				}
 			}
 		}
-		
-//		for(String term : index_for_query.keySet()){
-//			System.out.println(term + " " + index_for_query.get(term).getCount());
-//		}
+
+		// for(String term : index_for_query.keySet()){
+		// System.out.println(term + " " +
+		// index_for_query.get(term).getCount());
+		// }
 
 		// Temporary variables.
 		TermMetrics tm = null;
@@ -243,8 +252,8 @@ public class SearchEngine {
 		Double d_square;
 		ComparableEntity newEntity = null;
 
-
-		// Go through the index for the query and update it with TF, IDF, TF-IDF values.
+		// Go through the index for the query and update it with TF, IDF, TF-IDF
+		// values.
 		// Also, calculate ||Query|| which is required for cosine-similarity
 		for (Map.Entry<String, TermMetrics> entry : index_for_query.entrySet()) {
 			tm = entry.getValue();
@@ -254,34 +263,43 @@ public class SearchEngine {
 			entry.setValue(tm);
 		}
 		q_square = Math.sqrt(q_square);
-		
+
 		// To hold the sorted document list.
 		List<ComparableEntity> resultList = new ArrayList<ComparableEntity>();
 		
-		// A double loops to calculate the cosine similarity between the query and a relevant document
+		// A double loops to calculate the cosine similarity between the query
+		// and a relevant document
 		for (Posting posting : relevantDocuments) {
 			documentId = posting.getDocument().getId();
 			q_d_dot_product = 0.0;
-			d_square = 0.0;
-			for (Map.Entry<String, TermMetrics> entry : index_for_query.entrySet()) {
+			
+			d_square = Arrays.stream(termDocumentMapping_T[documentId])
+					.map(n -> n*n)
+					.reduce(0.0, Double::sum);
+			
+			d_square = Math.sqrt(d_square);
+			
+			for (Map.Entry<String, TermMetrics> entry : index_for_query
+					.entrySet()) {
 				tm = entry.getValue();
 				term = entry.getKey();
 				termId = term_identifier.get(term);
-				q_d_dot_product += (termDocumentMapping[termId][documentId] * tm.getTf_idf());
-				d_square += Math.pow(termDocumentMapping[termId][documentId],2);
+				q_d_dot_product += (termDocumentMapping[termId][documentId] * tm
+						.getTf_idf());
 			}
-			d_square = Math.sqrt(d_square);
-			newEntity = new ComparableEntity(posting.getDocument(), q_d_dot_product, d_square * q_square);
+			
+			newEntity = new ComparableEntity(posting.getDocument(),
+					q_d_dot_product, d_square * q_square);
 			resultList.add(newEntity);
 		}
-		
+
 		// Sort the results using comparator defined in ComparableEntity
-		if(resultList.size() > 0) {
+		if (resultList.size() > 0) {
 			Collections.sort(resultList);
-			
-			// To store the name of the documents 
+
+			// To store the name of the documents
 			List<String> result = new ArrayList<String>();
-			
+
 			for (ComparableEntity entity : resultList) {
 				// System.out.println(posting.getTf_idf());
 				result.add(entity.getDocument().getDocumentIdAsString());
@@ -332,7 +350,8 @@ public class SearchEngine {
 					}
 				}
 
-				// iterate through all documents to create an array of TF-IDF between each term and each document.
+				// iterate through all documents to create an array of TF-IDF
+				// between each term and each document.
 				searchEngine.iterateDocuments();
 
 				// Input scanner
@@ -379,32 +398,45 @@ public class SearchEngine {
 	}
 
 	private void iterateDocuments() {
-		termDocumentMapping = new double[term_identifier.size()][documents.size()];
+		termDocumentMapping = new double[term_identifier.size()][documents
+				.size()];
 		Integer termId;
-		
+
 		for (Map.Entry<String, List<Posting>> entry : index.entrySet()) {
 			termId = term_identifier.get(entry.getKey());
 			for (Posting posting : entry.getValue()) {
 				if (entry.getValue() != null) {
 					posting.setInverseDocFreq(documents.size(), entry
 							.getValue().size());
-					termDocumentMapping[termId][posting.getDocument()
-							.getId()] = posting.getTf_idf();
+					termDocumentMapping[termId][posting.getDocument().getId()] = posting
+							.getTf_idf();
 				}
 			}
 		}
-//		
-//		for(String term : term_identifier.keySet()){
+		
+		
+//		for (String term : term_identifier.keySet()) {
 //			System.out.println(term + " " + term_identifier.get(term));
 //		}
 //		System.out.println("\n");
-//		
-//		for(int i =0;i<term_identifier.size();i++) {
+
+//		for (int i = 0; i < term_identifier.size(); i++) {
 //			System.out.println();
-//			for(int j=0;j<documents.size();j++) {
+//			for (int j = 0; j < documents.size(); j++) {
 //				System.out.print(termDocumentMapping[i][j] + "\t\t\t");
 //			}
 //		}
 //		System.out.println();
+		
+		termDocumentMapping_T = MatrixUtils.createRealMatrix(termDocumentMapping).transpose().getData();
+		
+//		System.out.println("-------------------------------------\n");
+//
+//		for (int i = 0; i < documents.size(); i++) {
+//			System.out.println();
+//			for (int j = 0; j < term_identifier.size(); j++) {
+//				System.out.print(termDocumentMapping_T[i][j] + "\t\t\t");
+//			}
+//		}
 	}
 }
